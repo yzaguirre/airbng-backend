256a0f79f4e180543fbae8804597baa6e3d004ae
David Yzaguirre <dvdyzag@gmail.com>
2018-04-23 22:23:36 -0600

    Completed Chapter 04

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +15,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('listing/{listing}', 'ListingController@get_listing_api');
